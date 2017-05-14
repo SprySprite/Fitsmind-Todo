9651a66c5b40a17df334ba48245119b2c36bfec1
Edward Kwon <edwardkwon@Edwards-MacBook-Air.local>
2017-05-14 14:26:38 -0700

    Initial Commit

---
+++ SparkleDate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SparkleDate
+//
+//  Created by Edward Kwon on 5/14/17.
+//  Copyright © 2017 Spry Sprite LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
